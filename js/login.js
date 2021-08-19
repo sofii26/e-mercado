b9e15032bea6ff353a9f0eaba58222cc2f994221
sofii26 <sofiagutierrez2606@gmail.com>
2021-08-19 19:27:25 -0300

    login e imágenes

--- js/login.js
+++ js/login.js
@@ -1,6 +1,32 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function(){
+ document.getElementById("ingreso").addEventListener("click", function() {
 
-});
+     let inputEmail = document.getElementById("inputEmail");
+     let inputPassword = document.getElementById("inputPassword");
+     let campos = true;
+
+     if (inputEmail.value === "") {
+         inputEmail.classList.add("invalido");
+         campos = false;
+     }
+     else {
+         inputEmail.classList.remove("invalido");
+     }
+     if (inputPassword.value === "") {
+         inputPassword.classList.add("invalido");
+         campos = false;
+     }
+     else {
+         inputPassword.classList.remove("invalido");
+     }
+     if (campos) {
+         document.getElementById("ingreso").innerHTML = window.location = "inicio.html";
+     }
+     else { 
+         alert("Falta rellenar campos");
+     }
+ }
+)});
--- js/products.js
+++ js/products.js
@@ -1,6 +1,33 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+//elementos HTML presentes.  
+var prods = [];
 
+function MostrarProds(array){
+let lista= "";
+for(let i = 0; i < array.length; i++){ 
+    let productos = array[i];
+    lista += `
+    <div class: prodEstilo, style= "margin-left:30px">
+    <h2 style ="color: crimson; font-weight: bold"> ` + productos.name + ` </h2> "<br>"
+    <img src="` + productos.imgSrc + `" class="imagencitas"> "<br>"
+    <p> ` + 'Descripción: ' + productos.description + `</p>
+    <p> ` + 'Costo: ' + productos.cost + " " + productos.currency + ` </p>
+    <p> ` + 'Vendidos: ' + productos.soldCount + ` </p>
+   </div>
+    `
+    document.getElementById("prodHTML").innerHTML = lista;
+}
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+getJSONData(PRODUCTS_URL)
+.then(function(resultado) {
+    if(resultado.status === "ok")
+    {
+        prods = resultado.data;
+        MostrarProds(prods);
+
+    }
+});
 });
